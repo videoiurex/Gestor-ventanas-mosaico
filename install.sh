834b448146c20d43dd301c0bc181ddc823d9b543
videoiurex <videoiurex@gmail.com>
2024-08-27 09:42:55 +0200

    organizando en directorio

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Copiar ficheros y directorios a home
-cp -rv i3 ~/.config/
-cp -rv polybar ~/.config/
-mkdir -p ~/Imágenes/fondos
-cp -v fondo/fondo1.png ~/Imágenes/fondos
-mkdir -p  ~/.local/bin
-cp powermenu ~/.local/bin/
