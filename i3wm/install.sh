43afcf32dbe8cd2cd026465b07941689146138e5
videoiurex <videoiurex@gmail.com>
2024-08-27 09:13:09 +0200

    pasando todo git a un directorio

---
+++ i3wm/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Copiar ficheros y directorios a home
+cp -rv i3 ~/.config/
+cp -rv polybar ~/.config/
+mkdir -p ~/Imágenes/fondos
+cp -v fondo/fondo1.png ~/Imágenes/fondos
+mkdir -p  ~/.local/bin
+cp powermenu ~/.local/bin/
