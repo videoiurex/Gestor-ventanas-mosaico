2df95bdc74a72f4442747419d7839eefb51beae9
videoiurex <videoiurex@gmail.com>
2024-08-28 11:01:21 +0200

    añadiendo directorio de bspwm

---
+++ bspwm/install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+mkdir ~/.config/bspwm
+mkdir ~/.config/sxhkd
+mkdir ~/.config/polybar
+mkdir -p ~/Imágenes/fondos
+cp bspwmrc ~/.config/bspwm/bspwmrc
+cp sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp polybarconfig.ini ~/.config/polybar/config.ini
+cp fondo1.png ~/Imágenes/fondos/
