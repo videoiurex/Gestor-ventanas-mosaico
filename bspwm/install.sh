b6271e34e39559aba2e64d93606aebd0ab43337d
videoiurex <videoiurex@gmail.com>
2024-08-30 18:28:02 +0200

    añadido el powermenu

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -3,8 +3,10 @@ mkdir -p ~/.config/bspwm
 mkdir -p ~/.config/sxhkd
 mkdir -p ~/.config/polybar
 mkdir -p ~/Imágenes/fondos
+mkdir -p ~/.local/bin
 cp bspwmrc ~/.config/bspwm/bspwmrc
 cp sxhkdrc ~/.config/sxhkd/sxhkdrc
 cp polybarconfig.ini ~/.config/polybar/config.ini
 cp launch.sh ~/.config/polybar/
 cp fondo1.png ~/Imágenes/fondos/
+cp powermenu ~/.local/bin
