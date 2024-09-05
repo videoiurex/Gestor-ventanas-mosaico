3ca5e4fde0f34ef2dfd22deba90f9ac75332f2d7
videoiurex <videoiurex@gmail.com>
2024-09-05 16:52:49 +0200

    mas errores corregidos

--- bspwm/install.sh
+++ bspwm/install.sh
@@ -5,6 +5,6 @@ mkdir -p ~/Imágenes/fondos
 mkdir -p ~/.local/bin
 cp -v bspwmrc ~/.config/bspwm/
 cp -v sxhkdrc ~/.config/sxhkd/
-cp -v ../polybar/ ~/.config/ 
+cp -rv ../polybar/ ~/.config/ 
 cp -v bspwmfondo.png ~/Imágenes/fondos/
 cp -v ../powermenu ~/.local/bin/
