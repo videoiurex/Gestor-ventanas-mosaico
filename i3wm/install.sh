34ae2844edcdd31ba0167731404a35a5e1ef10a8
videoiurex <videoiurex@gmail.com>
2024-09-05 16:18:44 +0200

    corregidos errores en i3 install

--- i3wm/install.sh
+++ i3wm/install.sh
@@ -4,6 +4,6 @@ mkdir -vp ~/.config/i3
 mkdir -vp ~/Imágenes/fondos
 mkdir -vp  ~/.local/bin
 cp -v config ~/.config/i3/
-cp -v ../polybar ~/.config/
-cp -v fondo/i3wmfondo.png ~/Imágenes/fondos
+cp -rv ../polybar ~/.config/
+cp -v i3wmfondo.png ~/Imágenes/fondos
 cp ../powermenu ~/.local/bin/
